110ee50e5e243a12286dda0bfe96573a022489d5
Oliver <olimovier@gmail.com>
2019-07-01 17:06:54 +0800

    Try to create a table

---
+++ initdb/initdb.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
+  "fmt"
+)
+
+type Person struct {
+  Name string
+  Age int
+}
+
+var db *gorm.DB
+
+func main() {
+
+  username := "postgres"
+  password := "postgres" 
+	dbName := "my_database" 
+	dbHost := "db" 
+
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+	fmt.Println(dbUri)
+
+	conn, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db = conn
+	db.Debug().AutoMigrate(&Person{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
