e3d71cb9c787dabdd7db6baefc0468ede833d0f8
Oliver <olimovier@gmail.com>
2019-07-02 13:45:42 +0800

    Try to do some CURD operations

--- initdb/initdb.go
+++ initdb/initdb.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Person struct {
+  gorm.Model
   Name string
   Age int
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
   "github.com/gorilla/mux"
   "fmt"
   "net/http"
@@ -8,15 +10,32 @@ import (
 )
 
 type Person struct {
+  gorm.Model
   Name string
   Age int
 }
 
 func getVocabulary(w http.ResponseWriter, r *http.Request) {
-  d := Person{"Bobb", 24}
+  username := "postgres"
+  password := "postgres" 
+	dbName := "my_database" 
+	dbHost := "db" 
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+  
+  db, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		fmt.Print(err)
+  }
+  
+  db.Create(&Person{Name: "Peter", Age: 22})
+
+  var person []Person
+  db.Find(&person)
+
   w.Header().Set("Content-Type", "application/json")
   w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(d)
+	json.NewEncoder(w).Encode(person)
 }
 
 func main() {
