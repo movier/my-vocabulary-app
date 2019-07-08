bc5ea61d1b7d021b5a7995deda6df0e923c2adff
Oliver <olimovier@gmail.com>
2019-07-08 14:49:36 +0800

    We can upload file now

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
   "encoding/json"
   "app/models"
   "html/template"
+  "io/ioutil"
+  "os"
+  "log"
 )
 
 var db *gorm.DB
@@ -29,6 +32,51 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
   p := &models.Person{Name: "title", Age: 22}
   t, _ := template.ParseFiles("upload.html")
   t.Execute(w, p)
+}
+
+func receiveFile(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("File Upload Endpoint Hit")
+
+  // Parse our multipart form, 10 << 20 specifies a maximum
+  // upload of 10 MB files.
+  r.ParseMultipartForm(10 << 20)
+  // FormFile returns the first file for the given key `myFile`
+  // it also returns the FileHeader so we can get the Filename,
+  // the Header and the size of the file
+  file, handler, err := r.FormFile("myFile")
+  if err != nil {
+    fmt.Println("Error Retrieving the File")
+    fmt.Println(err)
+    return
+  }
+  defer file.Close()
+  fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+  fmt.Printf("File Size: %+v\n", handler.Size)
+  fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+  dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+  }
+  fmt.Printf("current dir: %+v\n", dir)
+  // Create a temporary file within our temp-images directory that follows
+  // a particular naming pattern
+  tempFile, err := ioutil.TempFile(dir + "/temp-images", "upload-*.png")
+  if err != nil {
+    fmt.Println("Create Error: %+v\n", err)
+  }
+  defer tempFile.Close()
+
+  // read all of the contents of our uploaded file into a
+  // byte array
+  fileBytes, err := ioutil.ReadAll(file)
+  if err != nil {
+    fmt.Println(err)
+  }
+  // write this byte array to our temporary file
+  tempFile.Write(fileBytes)
+  // return that we have successfully uploaded our file!
+  fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func main() {
@@ -53,6 +101,7 @@ func main() {
   router := mux.NewRouter()
   router.HandleFunc("/api/vocabulary", getVocabulary).Methods("GET")
   router.HandleFunc("/upload", uploadFile).Methods("GET")
+  router.HandleFunc("/upload", receiveFile).Methods("POST")
 
   port := "8080"
   fmt.Println(port)
